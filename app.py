84c83f031f4969c920b9728e49650ec8b5b6cac1
karushp <karushpradhan@gmail.com>
2024-06-15 23:41:40 +0900

    added streamlit python file

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
-import av
-import cv2
-from ultralytics import YOLO
 import numpy as np
-import threading
-import time
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # Import function from another data.py
-from data import history , country_performance_WC2014 , plot_goals_scored
+from data import country_performance_WC2014 , plot_goals_scored, display_goal_counts, history
 
 
-countries = ['Germany' , 'Scotland' , 'Hungary', 'Switzerland','Spain',
-             'Croatia', 'Italy','Albania','Slovenia','Denmark','Serbia',
-             'England', 'Netherlands','France','Poland','Austria',
-             'Ukraine','Slovakia','Belgium','Romania','Portugal','Czechia',
-             'Georgia','Türkiye']
+countries = ['Select Team', 'Albania', 'Austria', 'Belgium', 'Croatia',
+             'Czechia', 'Denmark', 'England', 'France', 'Georgia', 'Germany',
+             'Hungary', 'Italy', 'Netherlands', 'Poland', 'Portugal', 'Romania',
+             'Scotland', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Switzerland',
+             'Türkiye', 'Ukraine']
+
 
 # Set up the Streamlit app
 st.set_page_config(page_title="Euro Infographics", page_icon="⚽", layout="wide")
@@ -24,17 +21,32 @@ st.markdown("""
 This application shows the history between two selected teams.
 """)
 
-# Select teams
-team1 = st.selectbox("Select Team 1", countries)
-team2 = st.selectbox("Select Team 2", countries)
+# Create two columns for team selection
+col1, col2 = st.columns(2)
 
-# Apply 'Plot goals Scored' function on the selected teams
-if team1 and team2:
-    st.write(f"### Goals Analysis for {team1} and {team2}")
-    plot_goals_scored(team1, team2)
+with col1:
+    team1 = st.selectbox("Select Team 1", countries, index=0)
 
-# Apply 'Country Performance WC2014' function on each selected team
-if team1:
-    country_performance_WC2014(team1)
-if team2:
-    country_performance_WC2014(team2)
+with col2:
+    team2 = st.selectbox("Select Team 2", countries, index=0)
+
+
+#Add a button to trigger the analysis
+if st.button("Show Analysis"):
+    if team1 == 'Select Team' or team2 == 'Select Team':
+        st.write("Please select both teams to display the analysis.")
+    else:
+        # Apply Plot Goals Scored function on the selected teams
+        if team1 != 'Select team' or team2 != 'Select team':
+            st.write(f"### Historical Analysis for {team1 if team1 != 'Select Team' else ''} {'and' if team1 != 'Select Team' and team2 != 'Select Team' else ''} {team2 if team2 != 'Select Team' else ''}")
+            history(team1, team2)
+
+
+        # Apply Country Performance WC2014 function on each selected team
+        # Adding a frame around the plot
+        frame_style = {'width': '100%', 'padding': '20px', 'border': '1px solid #b0c4de', 'border-radius': '5px'}
+        with st.frame(style=frame_style):
+            if team1 != 'Select Team':
+                country_performance_WC2014(team1)
+            # if team2 != 'Select Team':
+            #     country_performance_WC2014(team2)
--- data.py
+++ data.py
@@ -2,37 +2,53 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import streamlit as st
 
-
-# Loding Euro Dataset
-euro_df = pd.read_csv('/data/Uefa Euro Cup All Matches.csv')
-
-# Function to clean Eruo Dataset
-def clean_euro_df(df):
-    # Clearing string for Home and Away team
-    df['HomeTeamName'] = df['HomeTeamName'].str.strip()
-    df['AwayTeamName'] = df['AwayTeamName'].str.strip()
-    df['SpecialWinConditions'] = df['SpecialWinConditions'].str.strip()
-
-    # Filling NULL value with NA
-    df['SpecialWinConditions'].fillna('NA',inplace=True)
-
-clean_euro_df(euro_df)
 
 # Function to show EURO history for 2 teams
-search_teams =
 def history(team1,team2):
+    # Loding Euro Dataset
+    euro_df = pd.read_csv('data/Uefa Euro Cup All Matches.csv')
+
+    # Function to clean Eruo Dataset
+    def clean_euro_df(df):
+        # Clearing string for Home and Away team
+        df['HomeTeamName'] = df['HomeTeamName'].str.strip()
+        df['AwayTeamName'] = df['AwayTeamName'].str.strip()
+        df['SpecialWinConditions'] = df['SpecialWinConditions'].str.strip()
+
+        # Filling NULL value with NA
+        df['SpecialWinConditions'].fillna('NA',inplace=True)
+
+    clean_euro_df(euro_df)
 
     mask = ((euro_df['HomeTeamName'] == team1) & (euro_df['AwayTeamName'] == team2)) | \
            ((euro_df['HomeTeamName'] == team2) & (euro_df['AwayTeamName'] == team1))
 
 
     result = euro_df[mask]
+    if result.empty:
+        st.write('No data Found')
+    else:
+        st.table(result)
 
-    return result
-
-
+# Function to plot goals scored between 2 teams
 def plot_goals_scored(team1, team2):
+    # Loding Euro Dataset
+    euro_df = pd.read_csv('data/Uefa Euro Cup All Matches.csv')
+
+    # Function to clean Eruo Dataset
+    def clean_euro_df(df):
+        # Clearing string for Home and Away team
+        df['HomeTeamName'] = df['HomeTeamName'].str.strip()
+        df['AwayTeamName'] = df['AwayTeamName'].str.strip()
+        df['SpecialWinConditions'] = df['SpecialWinConditions'].str.strip()
+
+        # Filling NULL value with NA
+        df['SpecialWinConditions'].fillna('NA',inplace=True)
+
+    clean_euro_df(euro_df)
+
     """
     Plots the goals scored by two teams in their respective matches.
 
@@ -42,7 +58,7 @@ def plot_goals_scored(team1, team2):
     matches (pd.DataFrame): DataFrame containing match data with columns 'HomeTeamName', 'AwayTeamName', 'Date', 'HomeTeamGoals', and 'AwayTeamGoals'.
     """
 
-    plt.figure(figsize=(10, 4))
+    plt.figure(figsize=(10, 8))
 
     # Goals scored by Team1
     team1_matches = euro_df[(euro_df['HomeTeamName'] == team1) | (euro_df['AwayTeamName'] == team1)]
@@ -67,18 +83,48 @@ def plot_goals_scored(team1, team2):
     plt.xticks(rotation=45)
     plt.legend()
     plt.tight_layout()
-    plt.show()
+    st.pyplot()
 
+# to show goal count
+def display_goal_counts(team1, team2):
+        # Loding Euro Dataset
+    euro_df = pd.read_csv('data/Uefa Euro Cup All Matches.csv')
 
+    # Function to clean Eruo Dataset
+    def clean_euro_df(df):
+        # Clearing string for Home and Away team
+        df['HomeTeamName'] = df['HomeTeamName'].str.strip()
+        df['AwayTeamName'] = df['AwayTeamName'].str.strip()
+        df['SpecialWinConditions'] = df['SpecialWinConditions'].str.strip()
 
-# Import Worldcup 2014 data
-worldcup14_df = pd.read_csv('/data/international-fifa-world-cup-2014-brazil-teams-2014-to-2014-stats.csv')
+        # Filling NULL value with NA
+        df['SpecialWinConditions'].fillna('NA',inplace=True)
 
-# Filter Worldcup 2014 data
-worldcup14_filter_df= worldcup14_df[['country','win_percentage','draw_percentage_overall','loss_percentage_ovearll']]
+    clean_euro_df(euro_df)
+
+    st.write(f"Goals scored by {team1}:")
+    team1_matches = euro_df[(euro_df['HomeTeamName'] == team1) | (euro_df['AwayTeamName'] == team1)]
+    home_goals_team1 = team1_matches['HomeTeamGoals'].sum()
+    away_goals_team1 = team1_matches['AwayTeamGoals'].sum()
+    st.write(f"Total goals at home: {home_goals_team1}")
+    st.write(f"Total goals away: {away_goals_team1}")
+
+    st.write(f"Goals scored by {team2}:")
+    team2_matches = euro_df[(euro_df['HomeTeamName'] == team2) | (euro_df['AwayTeamName'] == team2)]
+    home_goals_team2 = team2_matches['HomeTeamGoals'].sum()
+    away_goals_team2 = team2_matches['AwayTeamGoals'].sum()
+    st.write(f"Total goals at home: {home_goals_team2}")
+    st.write(f"Total goals away: {away_goals_team2}")
+
 
 # Function to filter and plot data for a specific country
 def country_performance_WC2014(country):
+    # Import Worldcup 2014 data
+    worldcup14_df = pd.read_csv('data/international-fifa-world-cup-2014-brazil-teams-2014-to-2014-stats.csv')
+
+    # Filter Worldcup 2014 data
+    worldcup14_filter_df= worldcup14_df[['country','win_percentage','draw_percentage_overall','loss_percentage_ovearll']]
+
     # Filter the DataFrame for the specific country
     country_data = worldcup14_filter_df[worldcup14_filter_df['country'] == country]
 
@@ -93,7 +139,7 @@ def country_performance_WC2014(country):
     labels = ['Win', 'Lose', 'Draw']
 
     # Create the bar plot
-    plt.figure(figsize=(8, 5))
+    plt.figure(figsize=(5, 3))
     plt.bar(labels, percentages, color=['green', 'red', 'blue'])
 
     # Add title and labels
@@ -102,4 +148,5 @@ def country_performance_WC2014(country):
     plt.xlabel('Outcome')
 
     # Show the plot
-    plt.show()
+    st.pyplot()
+    st.set_option('deprecation.showPyplotGlobalUse', False)
